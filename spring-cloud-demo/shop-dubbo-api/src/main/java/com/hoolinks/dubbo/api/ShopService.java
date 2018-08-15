5124241bb4b9daa88426f3f87ca38617a3583288
zzp <354697171@qq.com>
2018-08-15 17:34:13 +0800

    修改注释

--- spring-cloud-demo/shop-dubbo-api/src/main/java/com/hoolinks/dubbo/api/ShopService.java
+++ spring-cloud-demo/shop-dubbo-api/src/main/java/com/hoolinks/dubbo/api/ShopService.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+ * 
+ * @author zzp
+ *
+ */
 @FeignClient("SHOP-DUBBO-PROVIDER")
 public interface ShopService {
 	

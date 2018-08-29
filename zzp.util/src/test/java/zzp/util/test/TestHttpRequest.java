6fa0d9d2143c63aee1b9a835456dd91730fce141
zzp <354697171@qq.com>
2018-08-29 17:20:04 +0800

    修改测试类

--- zzp.util/src/test/java/zzp/util/test/TestHttpRequest.java
+++ zzp.util/src/test/java/zzp/util/test/TestHttpRequest.java
@@ -23,17 +23,25 @@ public class TestHttpRequest {
 //		Map<String, String> params = new HashMap<String, String>();
 //		params.put("method", "GetVehicleNoList");
 //		params.put("appkey", "30914b89-262a-4832-b8b9-fe33770b4b4d");
-//		params.put("timestamp", "2018-08-29 10:13:36");
+//		params.put("timestamp", smft.format(new Date()));
 //		params.put("format", "json");
 //		String result = HttpRequest.sendGet("http://api.e6gps.com/public/v3/StatisticsReport/Call", "69411cff-d7be-4458-952f-2fca0267b408", params);
 //		System.out.println(result);
 //		
-//		Map map1 = JSON.parseObject(result);
-//		System.out.println(map1);
-//		Map<String, Object> map2 = (Map<String, Object>)map1;
-//		for (Map.Entry<String,Object> entry : map2.entrySet()) {
-//			System.out.println(entry.getKey()+"--->"+entry.getValue());
+//		JSONObject jsonObject = JSONObject.parseObject(result);
+//		JSONObject resultJson = jsonObject.getJSONObject("result");
+//		JSONArray datas = resultJson.getJSONArray("data");
+//		if (datas != null && datas.size() > 0) {
+//			StringBuffer sBuffer = new StringBuffer();
+//			for (int i = 0; i < datas.size(); i++) {
+//				JSONObject data = datas.getJSONObject(i);
+//				String vehicleNo = data.getString("VehicleNo");
+//				sBuffer.append(vehicleNo + ",");
+//			}
+//			sBuffer.deleteCharAt(sBuffer.lastIndexOf(","));
+//			System.out.println(sBuffer.toString());
 //		}
+		
 		
 		//4.2 获取车辆的最新位置信息 
 		Map<String, String> params = new HashMap<String, String>();
@@ -43,7 +51,7 @@ public class TestHttpRequest {
 		params.put("format", "json");
 		params.put("isoffsetlonlat", "2");
 		params.put("sessionid", "");
-		params.put("vehicle", "粤BAD548");
+		params.put("vehicle", "粤BZ1291");
 		String result = HttpRequest.sendGet("http://api.e6gps.com/public/v3/Inface/Call", "69411cff-d7be-4458-952f-2fca0267b408", params);
 		System.out.println(result);
 		

c3c7fe45efdb8253b875fb7122a0edd09b996e9b
zzp <zzp@zzp-X6>
2018-09-03 21:35:54 +0800

    请求接口

---
+++ zzp.util/src/main/java/com/repeat/request/Request.java
@@ -0,0 +1,17 @@
+package com.repeat.request;
+
+/**
+ * 请求接口
+ * 
+ * @author karyzeng
+ * @since 2018.09.03
+ *
+ */
+public interface Request {
+
+	/**
+	 * 请求方法
+	 * @return
+	 */
+	String requestMethod();
+}

d69d66a1ca166510c665f7cfa35e3f7632415972
zzp <354697171@qq.com>
2018-09-20 16:56:29 +0800

    添加trimSpace方法

--- zzp.util/src/main/java/com/util/convert/ConvertUtil.java
+++ zzp.util/src/main/java/com/util/convert/ConvertUtil.java
@@ -14,6 +14,13 @@ public class ConvertUtil {
 	
 	private static SimpleDateFormat smft = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
+	/**
+	 * 将各种类型的数据转换成用原生SQL的insert语句可以用的value
+	 * 
+	 * @param t 
+	 * 
+	 * @return 如果t为null，则返回"NULL"对应SQL的NULL，如果不为空，则将其转换为"'value'"的格式
+	 */
 	public static <T> String convertInsertParam(T t) {
 		if (t != null) {
 			Class clazz = t.getClass();
@@ -28,6 +35,21 @@ public class ConvertUtil {
 		return "NULL";
 	}
 	
+	/**
+	 * 1、去除字符串的空格，制表符，换行等符号
+	 * 2、将中文逗号"，"转换成","
+	 * 
+	 * @param str
+	 * @return String
+	 */
+	public static String trimSpace(String str) {
+    	if (str == null) return null;
+		String tempVal = str.trim();
+		tempVal = tempVal.replaceAll("，", ",");//处理中文逗号
+		tempVal = tempVal.replaceAll("\\r|\\n|\\s|\\t", "");//去掉换行和字符串中间空格
+		return tempVal;
+	}
+	
 	public static void main(String[] args) {
 		int a = 12;
 		String b = "zzz";
@@ -38,6 +60,9 @@ public class ConvertUtil {
 		System.out.println(convertInsertParam(date));
 		System.out.println(convertInsertParam(d));
 		System.out.println(convertInsertParam(null));
+		
+		//=========================
+		System.out.println(trimSpace("  dd  zzz  dddgg   				"));
 	}
 	
 }

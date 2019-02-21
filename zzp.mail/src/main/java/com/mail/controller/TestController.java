a0c8a084002f882f4623cd3ec081d3d580decd4e
karyzeng <354697171@qq.com>
2019-02-21 10:42:26 +0800

    修改代码

--- zzp.mail/src/main/java/com/mail/controller/TestController.java
+++ zzp.mail/src/main/java/com/mail/controller/TestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
 import com.mail.util.MailSenderInfo;
 import com.mail.util.SimpleMailSender;
 
@@ -30,13 +31,15 @@ public class TestController {
 		MailSenderInfo mailInfo = new MailSenderInfo();   
 	    mailInfo.setMailServerHost("mail.ttni.com.cn");   
 	    mailInfo.setMailServerPort("587");   
-	    mailInfo.setValidate(true);   
+	    mailInfo.setValidate(true);
+	    mailInfo.setTrust(false);
 	    mailInfo.setUserName("ttgclogistics_service@gzcn.toyotsu.net");   
 	    mailInfo.setPassword("0110@CQY8#");//您的邮箱密码   
 	    mailInfo.setFromAddress("ttgclogistics_service@gzcn.toyotsu.net");   
-	    mailInfo.setToAddress("354697171@qq.com");   
+	    mailInfo.setToAddress("zengzhenpeng@hoolinks.com");   
 	    mailInfo.setSubject("test");   
-	    mailInfo.setContent(content);   
+	    mailInfo.setContent(content);
+	    System.out.println("sendMailOld发送邮件内容：" + JSON.toJSONString(mailInfo));
 	    //这个类主要来发送邮件  
 	    SimpleMailSender sms = new SimpleMailSender();  
 	    boolean textMail = sms.sendTextMail(mailInfo);//发送文体格式   
@@ -51,15 +54,16 @@ public class TestController {
 	public String sendMailNew(@RequestParam(value = "content",required = true)String content){
 		MailSenderInfo mailInfo = new MailSenderInfo();   
 	    mailInfo.setMailServerHost("smtp.toyotsu-ea.com");   
-	    mailInfo.setMailServerPort("25");   
+	    mailInfo.setMailServerPort("587");   
 	    mailInfo.setValidate(true);
 	    mailInfo.setTrust(true);
 	    mailInfo.setUserName("ttgclogistics_service@sys.toyotsu-ea.com");   
 	    mailInfo.setPassword("TTCChina_476");//您的邮箱密码   
 	    mailInfo.setFromAddress("ttgclogistics_service@sys.toyotsu-ea.com");   
-	    mailInfo.setToAddress("354697171@qq.com");   
+	    mailInfo.setToAddress("zengzhenpeng@hoolinks.com");   
 	    mailInfo.setSubject("test");   
-	    mailInfo.setContent(content);   
+	    mailInfo.setContent(content);
+	    System.out.println("sendMailNew发送邮件内容：" + JSON.toJSONString(mailInfo));
 	    //这个类主要来发送邮件  
 	    SimpleMailSender sms = new SimpleMailSender();  
 	    boolean textMail = sms.sendTextMail(mailInfo);//发送文体格式   

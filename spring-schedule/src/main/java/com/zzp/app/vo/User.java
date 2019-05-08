33f6a2fb4e7164799781ce5410411e173c58e1fb
karyzeng <354697171@qq.com>
2019-05-08 18:44:39 +0800

    用户信息vo
    Signed-off-by: karyzeng <354697171@qq.com>

---
+++ spring-schedule/src/main/java/com/zzp/app/vo/User.java
@@ -0,0 +1,32 @@
+package com.zzp.app.vo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @Description TODO
+ * @Author karyzeng
+ * @since 2019.05.08
+ **/
+@Data
+public class User implements Serializable{
+
+    private static final long serialVersionUID = -1L;
+
+    /**
+     * id
+     */
+    private Integer id;
+
+    /**
+     * 用户名
+     */
+    private String userName;
+
+    /**
+     * 登录名
+     */
+    private String loginId;
+
+}

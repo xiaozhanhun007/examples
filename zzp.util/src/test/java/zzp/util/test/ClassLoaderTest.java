5720d387cd75b6a5b574d4fbe1528b5236fa7b07
karyzeng <354697171@qq.com>
2019-06-04 13:45:02 +0800

    类加载器测试类
    Signed-off-by: karyzeng <354697171@qq.com>

--- zzp.util/src/test/java/zzp/util/test/ClassLoaderTest.java
+++ zzp.util/src/test/java/zzp/util/test/ClassLoaderTest.java
@@ -3,6 +3,14 @@ package zzp.util.test;
 import com.excel.util.ParseXMLUtil;
 import com.util.http.MD5Util;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.Collectors;
+
 public class ClassLoaderTest {
 
     public static void main(String[] args) {
@@ -14,6 +22,30 @@ public class ClassLoaderTest {
 
         System.out.println(ParseXMLUtil.class.getClassLoader().getResource(""));
 
+        Queue<Integer> queue = new PriorityQueue<Integer>();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(3);
+        System.out.println(queue);
+
+        Lock lock = new ReentrantLock();
+
+        String str = "Could not read JSON: Unrecognized field \"entryDclTimeStart\" (class com.hoolinks.jg.dcl.vo.InvtHeadQuery), not marked as ignorable (21 known properties: \"size\", \"descs\", \"putrecNo\", \"invtDclTimeEnd\", \"suppliersCustomsCode\", \"companyCode\", \"invtDclTimeStart\", \"supvModecd\", \"groupUid\", \"companyUid\", \"impexpPortcd\", \"dataStat\", \"masterCuscd\", \"createrName\", \"ascs\", \"bondInvtNo\", \"entBillDocNo\", \"createTime\", \"groupName\", \"updateTime\", \"current\"])";
+        String tempStr = str.substring(str.indexOf("\"") + 1);
+        String tempStr2 = tempStr.substring(0, tempStr.indexOf("\""));
+        System.out.println(tempStr);
+        System.out.println(tempStr2);
+
+
+        try {
+            long startTime = System.currentTimeMillis();
+            Thread.sleep(5);
+            long endTime = System.currentTimeMillis();
+            System.out.println("时间间隔：" + (endTime - startTime) +"毫秒");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 }

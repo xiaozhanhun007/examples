ffaa40481a51487e3c8fb6e828d0dbd9778702d0
karyzeng <354697171@qq.com>
2019-06-05 17:54:29 +0800

    死锁测试类
    Signed-off-by: karyzeng <354697171@qq.com>

---
+++ zzp.util/src/test/java/com/deadlock/util/test/DeadlockTest.java
@@ -0,0 +1,39 @@
+package com.deadlock.util.test;
+
+import com.deadlock.util.Account;
+
+/**
+ * @Description 死锁测试类
+ * @Author karyzeng
+ * @since 2019.06.05
+ **/
+public class DeadlockTest {
+
+    public static void main(String[] args) {
+        final Account accountA = new Account(200);
+        final Account accountB = new Account(300);
+
+        Thread threadA = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                accountA.transfer(accountB, 100);
+            }
+        });
+
+        Thread threadB = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                accountB.transfer(accountA, 150);
+            }
+        });
+
+        threadA.start();
+        threadB.start();
+
+        System.out.println("accountA余额：" + accountA.getBalance());
+        System.out.println("accountB余额：" + accountB.getBalance());
+
+        System.out.println("结束");
+    }
+
+}

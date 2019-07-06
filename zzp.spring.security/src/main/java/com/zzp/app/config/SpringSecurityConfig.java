c3778675f5e97386b5594c575597ef12335e2d08
zzp <354697171@qq.com>
2019-07-06 17:15:45 +0800

    修改configure方法

--- zzp.spring.security/src/main/java/com/zzp/app/config/SpringSecurityConfig.java
+++ zzp.spring.security/src/main/java/com/zzp/app/config/SpringSecurityConfig.java
@@ -1,11 +1,17 @@
 package com.zzp.app.config;
 
+import com.zzp.app.sms.SmsAuthenticationConfig;
+import com.zzp.app.sms.SmsCodeFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.social.connect.web.HttpSessionSessionStrategy;
+import org.springframework.social.connect.web.SessionStrategy;
 
 /**
  * @Description TODO
@@ -15,22 +21,35 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private SmsCodeFilter smsCodeFilter;
+
+    @Autowired
+    private SmsAuthenticationConfig smsAuthenticationConfig;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SessionStrategy sessionStrategy(){
+        return new HttpSessionSessionStrategy();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
+        http.addFilterBefore(smsCodeFilter, UsernamePasswordAuthenticationFilter.class)
+                .formLogin()
                 .loginPage("/login.html")
                 .loginProcessingUrl("/login")
                 .and()
                 .authorizeRequests()
-                .antMatchers("/login.html").permitAll()
+                .antMatchers("/login.html", "/smsCodeLogin.html", "/smsCode/get").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
-                .csrf().disable();
+                .csrf().disable()
+                .apply(smsAuthenticationConfig);
     }
 }

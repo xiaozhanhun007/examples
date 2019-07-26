cc333ce99c696a8c021d0d43e760162b37d69d28
karyzeng <354697171@qq.com>
2019-07-26 16:15:04 +0800

    直接插入排序
    Signed-off-by: karyzeng <354697171@qq.com>

---
+++ zzp.util/src/main/java/com/util/sort/DirectInsertSort.java
@@ -0,0 +1,61 @@
+package com.util.sort;
+
+/**
+ * @Description 直接插入排序
+ * @Author karyzeng
+ * @since 2019.07.26
+ **/
+public class DirectInsertSort {
+
+    /**
+     * 直接插入排序
+     *
+     * 时间复杂度：最坏O(n^2)，最好O(n)，平均O(n^2)
+     * 空间复杂度：O(n)
+     *
+     * @param array 要排序的数组
+     * @param sortType 排序类型
+     * @return
+     */
+    public static int[] directInsertSort(int[] array, String sortType) {
+        if (array != null && array.length > 0) {
+            int l = 1;// 默认第一个数为已排序好的列表，那么已排序好的列表长度默认为1
+            int i = 1;// 未排序好的列表起始位置为1
+            for (; i < array.length; i++) {
+                for (int j = 0; j < l; j++) {
+                    if (SortTypeEnum.ASC.getCode().equals(sortType)) {
+                        if (array[i] < array[j]) {
+                            // 升序排序
+                            int temp = array[j];
+                            array[j] = array[i];
+                            array[i] = temp;
+                        }
+                    } else {
+                        if (array[i] > array[j]) {
+                            // 升序排序
+                            int temp = array[j];
+                            array[j] = array[i];
+                            array[i] = temp;
+                        }
+                    }
+                }
+                l++;
+            }
+            return array;
+        }
+        return null;
+    }
+
+    public static void print(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{5, 76, 12, 95, 45, 23, 109, 64, 99};
+        directInsertSort(array, SortTypeEnum.DESC.getCode());
+        print(array);
+    }
+
+}

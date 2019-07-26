7239988d0c8bce1160da7cf84dc008e849107020
karyzeng <354697171@qq.com>
2019-07-26 15:19:02 +0800

    GPS对象
    Signed-off-by: karyzeng <354697171@qq.com>

---
+++ zzp.util/src/main/java/com/gps/util/Position.java
@@ -0,0 +1,29 @@
+package com.gps.util;
+
+/**
+ * @Description GPS对象
+ * @Author karyzeng
+ * @since 2019.04.19
+ **/
+public class Position {
+
+    private String lat;
+
+    private String lng;
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+}

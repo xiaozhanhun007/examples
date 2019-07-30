229ff3e289431f8030ea8236bd224047b41de244
karyzeng <354697171@qq.com>
2019-07-30 17:29:50 +0800

    修改print方法
    Signed-off-by: karyzeng <354697171@qq.com>

--- zzp.util/src/main/java/com/util/sort/ArrayUtil.java
+++ zzp.util/src/main/java/com/util/sort/ArrayUtil.java
@@ -15,6 +15,7 @@ public class ArrayUtil {
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
+        System.out.println("");
     }
 
     /**

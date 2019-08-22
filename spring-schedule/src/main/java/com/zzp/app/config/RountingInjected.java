56fc3d226298ba874ec7cfb3844f4f6abe261fd7
karyzeng <354697171@qq.com>
2019-08-22 17:44:45 +0800

    RountingInjected
    Signed-off-by: karyzeng <354697171@qq.com>

---
+++ spring-schedule/src/main/java/com/zzp/app/config/RountingInjected.java
@@ -0,0 +1,19 @@
+package com.zzp.app.config;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface RountingInjected {
+
+    String value() default "";
+
+}

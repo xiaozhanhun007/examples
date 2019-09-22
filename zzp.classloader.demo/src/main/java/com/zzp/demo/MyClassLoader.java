d7f0f67fcb0f5d7098a4327961ed4b8dcce1503c
zzp <354697171@qq.com>
2019-09-22 16:33:02 +0800

    重载loadClass方法

--- zzp.classloader.demo/src/main/java/com/zzp/demo/MyClassLoader.java
+++ zzp.classloader.demo/src/main/java/com/zzp/demo/MyClassLoader.java
@@ -11,17 +11,30 @@ public class MyClassLoader extends ClassLoader {
 
     @Override
     public Class<?> loadClass(String name) throws ClassNotFoundException {
+        return loadClass(name, false);
+    }
+
+    @Override
+    protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
         try {
-            String fileName = name.substring(name.lastIndexOf(".") + 1) + ".class";
-            // 当前类加载器先去加载指定的类
-            InputStream inputStream = getClass().getResourceAsStream(fileName);
-            if (inputStream == null) {
-                // 如果当前类加载器没有加载到类，则委托父类去加载
-                return super.loadClass(name);
+            // 先查找是否已经存在该类
+            Class clazz = findLoadedClass(name);
+            if (clazz == null) {
+                String fileName = name.substring(name.lastIndexOf(".") + 1) + ".class";
+                // 当前类加载器先去加载指定的类
+                InputStream inputStream = getClass().getResourceAsStream(fileName);
+                if (inputStream == null) {
+                    // 如果当前类加载器没有加载到类，则委托父类去加载
+                    return super.loadClass(name, resolve);
+                }
+                byte[] bytes = new byte[inputStream.available()];
+                inputStream.read(bytes);
+                clazz = defineClass(name, bytes, 0, bytes.length);
+
+                if (resolve) {
+                    resolveClass(clazz);
+                }
             }
-            byte[] bytes = new byte[inputStream.available()];
-            inputStream.read(bytes);
-            Class clazz = defineClass(name, bytes, 0, bytes.length);
             return clazz;
         } catch (Exception e) {
             e.printStackTrace();

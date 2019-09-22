9b394f31493aed531cd1d715e9360b0bc91e0e1a
karyzeng <354697171@qq.com>
2019-09-22 10:32:33 +0800

    自定义类加载器
    Signed-off-by: karyzeng <354697171@qq.com>

--- zzp.classloader.demo/src/main/java/com/zzp/demo/MyClassLoader.java
+++ zzp.classloader.demo/src/main/java/com/zzp/demo/MyClassLoader.java
@@ -1,9 +1,31 @@
 package com.zzp.demo;
 
+import java.io.InputStream;
+
+/**
+ * 自定义类加载器
+ * @author karyZeng
+ * @since 2019.09.22
+ */
 public class MyClassLoader extends ClassLoader {
 
     @Override
-    protected Class<?> findClass(String name) throws ClassNotFoundException {
-        return super.findClass(name);
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        try {
+            String fileName = name.substring(name.lastIndexOf(".") + 1) + ".class";
+            // 当前类加载器先去加载指定的类
+            InputStream inputStream = getClass().getResourceAsStream(fileName);
+            if (inputStream == null) {
+                // 如果当前类加载器没有加载到类，则委托父类去加载
+                return super.loadClass(name);
+            }
+            byte[] bytes = new byte[inputStream.available()];
+            inputStream.read(bytes);
+            Class clazz = defineClass(name, bytes, 0, bytes.length);
+            return clazz;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }

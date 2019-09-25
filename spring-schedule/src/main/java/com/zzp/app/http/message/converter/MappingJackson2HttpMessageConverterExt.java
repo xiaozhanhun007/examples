42203521c1a270fe09aa7b2f20992be226479c08
karyzeng <354697171@qq.com>
2019-09-25 13:37:53 +0800

    SpringMvc自定义反序列化转换器
    Signed-off-by: karyzeng <354697171@qq.com>

---
+++ spring-schedule/src/main/java/com/zzp/app/http/message/converter/MappingJackson2HttpMessageConverterExt.java
@@ -0,0 +1,40 @@
+package com.zzp.app.http.message.converter;
+
+import com.fasterxml.jackson.databind.JavaType;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.nio.charset.Charset;
+
+/**
+ * @Description SpringMvc自定义反序列化转换器
+ * @Author karyzeng
+ * @since 2019.09.24
+ **/
+public class MappingJackson2HttpMessageConverterExt extends MappingJackson2HttpMessageConverter {
+
+
+    public MappingJackson2HttpMessageConverterExt() {
+        super();
+    }
+
+    @Override
+    public Object read(Type type, Class<?> contextClass, HttpInputMessage inputMessage) throws IOException, HttpMessageNotReadableException {
+        MediaType contentType = inputMessage.getHeaders().getContentType();
+        Charset charset = contentType.getCharSet() != null ? contentType.getCharSet() : DEFAULT_CHARSET;
+        String content = FileCopyUtils.copyToString(new InputStreamReader(inputMessage.getBody(), charset));
+        String contentTypeStr = contentType.getType() + "/" + contentType.getSubtype();
+        if (StringUtils.isNotBlank(contentTypeStr) && contentTypeStr.equals("application/json")) {
+            content = content.replace("\"\"", "null");
+        }
+        JavaType javaType = getJavaType(type, contextClass);
+        return super.getObjectMapper().readValue(content, javaType);
+    }
+}

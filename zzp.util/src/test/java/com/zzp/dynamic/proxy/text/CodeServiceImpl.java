be88b63e507b6f4d08a65566ccde59b6ae37e538
zzp <354697171@qq.com>
2019-10-27 22:01:33 +0800

    实现类

---
+++ zzp.util/src/test/java/com/zzp/dynamic/proxy/text/CodeServiceImpl.java
@@ -0,0 +1,14 @@
+package com.zzp.dynamic.proxy.text;
+
+/**
+ * 实现类
+ * @author Garyzeng
+ * @since 2019.10.27
+ */
+public class CodeServiceImpl implements CodeService {
+
+    @Override
+    public void codeJava() {
+        System.out.println("编写Java程序");
+    }
+}

03ee73195ecd8149a4968b2ceb2b67ddc3963e8f
zzp <354697171@qq.com>
2019-10-27 22:01:03 +0800

    代理测试类

---
+++ zzp.util/src/test/java/com/zzp/dynamic/proxy/text/CodeProxyTest.java
@@ -0,0 +1,18 @@
+package com.zzp.dynamic.proxy.text;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * 代理测试类
+ * @author Garyzeng
+ * @since 2019.10.27
+ */
+public class CodeProxyTest {
+
+    public static void main(String[] args) {
+        CodeInvocationHandler invocationHandler = new CodeInvocationHandler(new CodeServiceImpl());
+        CodeService codeServiceProxy = (CodeService) Proxy.newProxyInstance(CodeProxyTest.class.getClassLoader(), new Class[]{CodeService.class}, invocationHandler);
+        codeServiceProxy.codeJava();
+    }
+
+}

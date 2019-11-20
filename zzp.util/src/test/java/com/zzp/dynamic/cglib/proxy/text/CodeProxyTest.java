480cb2df0f8ac5da4fc65759390df8f800e4c946
karyzeng <354697171@qq.com>
2019-11-20 11:17:22 +0800

    修改main方法
    Signed-off-by: karyzeng <354697171@qq.com>

--- zzp.util/src/test/java/com/zzp/dynamic/cglib/proxy/text/CodeProxyTest.java
+++ zzp.util/src/test/java/com/zzp/dynamic/cglib/proxy/text/CodeProxyTest.java
@@ -12,7 +12,7 @@ public class CodeProxyTest {
 
     public static void main(String[] args) {
         // 将生成的代理类存到本地
-        System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "E:\\zzp\\proxy");
+        System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "E:\\zzp\\ideaIU-2018.3.4.win\\idea-project\\examples\\com\\cglib");
         Enhancer enhancer = new Enhancer();
         enhancer.setSuperclass(CodeService.class);
         enhancer.setCallback(new CodeMethodInterceptor());

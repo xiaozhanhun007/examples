38a307a35ccc39e1685c7f4b92704dab3e454d1a
zzp <354697171@qq.com>
2019-11-21 06:22:32 +0800

    小米手机1代

---
+++ zzp.design.pattern/src/main/java/com/zzp/creation/factory/XiaomiPhone1.java
@@ -0,0 +1,15 @@
+package com.zzp.creation.factory;
+
+import com.zzp.creation.simple.factory.Phone;
+
+/**
+ * @Description 小米手机1代
+ * @Author Garyzeng
+ * @since 2019.11.20
+ **/
+public class XiaomiPhone1 implements Phone{
+
+    public void showName() {
+        System.out.println("小米手机1代");
+    }
+}

3cac85c9c1a1db547353752be1acbd20c1aaf26d
zzp <354697171@qq.com>
2019-11-21 06:24:07 +0800

    小米手机

---
+++ zzp.design.pattern/src/main/java/com/zzp/creation/simple/factory/XiaomiPhone.java
@@ -0,0 +1,13 @@
+package com.zzp.creation.simple.factory;
+
+/**
+ * @Description 小米手机
+ * @Author Garyzeng
+ * @since 2019.11.20
+ **/
+public class XiaomiPhone implements Phone{
+
+    public void showName() {
+        System.out.println("小米手机");
+    }
+}

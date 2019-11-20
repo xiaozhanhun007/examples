8680588387b622c38ed9995f49af18854eacaaf2
zzp <354697171@qq.com>
2019-11-21 06:23:52 +0800

    简单工厂模式

--- zzp.design.pattern/src/main/java/com/zzp/creation/simple/factory/SimpleFactory.java
+++ zzp.design.pattern/src/main/java/com/zzp/creation/simple/factory/SimpleFactory.java
@@ -6,4 +6,26 @@ package com.zzp.creation.simple.factory;
  * @since 2019.11.20
  **/
 public class SimpleFactory {
+
+    public Phone getPhone(String name) {
+        if (name.equals("xiaomi")) {
+            return new XiaomiPhone();
+        } else if (name.equals("huawei")) {
+            return new HuaweiPhone();
+        } else if (name.equals("apple")) {
+            return new ApplePhone();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        SimpleFactory factory = new SimpleFactory();
+        Phone xiaomi = factory.getPhone("xiaomi");
+        Phone huawei = factory.getPhone("huawei");
+        Phone apple = factory.getPhone("apple");
+        xiaomi.showName();
+        huawei.showName();
+        apple.showName();
+    }
+
 }

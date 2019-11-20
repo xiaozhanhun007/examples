defe56dbb8bdfaafb0a224a848d795c63ea16814
zzp <354697171@qq.com>
2019-11-21 06:23:30 +0800

    华为手机

---
+++ zzp.design.pattern/src/main/java/com/zzp/creation/simple/factory/HuaweiPhone.java
@@ -0,0 +1,13 @@
+package com.zzp.creation.simple.factory;
+
+/**
+ * @Description 华为手机
+ * @Author Garyzeng
+ * @since 2019.11.20
+ **/
+public class HuaweiPhone implements Phone {
+
+    public void showName() {
+        System.out.println("华为手机");
+    }
+}

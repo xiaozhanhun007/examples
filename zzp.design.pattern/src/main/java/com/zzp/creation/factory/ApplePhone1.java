2834ff4489b5950d0ae7ae6364fee9eb75437962
zzp <354697171@qq.com>
2019-11-21 06:21:03 +0800

    苹果手机1代

---
+++ zzp.design.pattern/src/main/java/com/zzp/creation/factory/ApplePhone1.java
@@ -0,0 +1,15 @@
+package com.zzp.creation.factory;
+
+import com.zzp.creation.simple.factory.Phone;
+
+/**
+ * @Description 苹果手机1代
+ * @Author Garyzeng
+ * @since 2019.11.20
+ **/
+public class ApplePhone1 implements Phone {
+
+    public void showName() {
+        System.out.println("苹果手机1代");
+    }
+}

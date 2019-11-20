b804e6f83c1c858c7357f1f22e6046205aaead0f
zzp <354697171@qq.com>
2019-11-21 06:23:15 +0800

    苹果手机

---
+++ zzp.design.pattern/src/main/java/com/zzp/creation/simple/factory/ApplePhone.java
@@ -0,0 +1,13 @@
+package com.zzp.creation.simple.factory;
+
+/**
+ * @Description 苹果手机
+ * @Author Garyzeng
+ * @since 2019.11.20
+ **/
+public class ApplePhone implements Phone {
+
+    public void showName() {
+        System.out.println("苹果手机");
+    }
+}

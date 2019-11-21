e005367a3fb0db7b83b6dffbeac7cd8b6d6368ca
karyzeng <354697171@qq.com>
2019-11-21 10:37:03 +0800

    苹果电脑
    Signed-off-by: karyzeng <354697171@qq.com>

---
+++ zzp.design.pattern/src/main/java/com/zzp/creation/abs/factory/AppleComputer.java
@@ -0,0 +1,13 @@
+package com.zzp.creation.abs.factory;
+
+/**
+ * @Description 苹果电脑
+ * @Author Garyzeng
+ * @since 2019.11.21
+ **/
+public class AppleComputer implements Computer {
+
+    public void showName() {
+        System.out.println("苹果电脑");
+    }
+}

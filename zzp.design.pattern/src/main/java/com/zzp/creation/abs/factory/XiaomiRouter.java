5dc56ae118e2748595bdd8561c805148d13db3ac
karyzeng <354697171@qq.com>
2019-11-21 10:38:58 +0800

    小米路由器
    Signed-off-by: karyzeng <354697171@qq.com>

---
+++ zzp.design.pattern/src/main/java/com/zzp/creation/abs/factory/XiaomiRouter.java
@@ -0,0 +1,13 @@
+package com.zzp.creation.abs.factory;
+
+/**
+ * @Description 小米路由器
+ * @Author Garyzeng
+ * @since 2019.11.21
+ **/
+public class XiaomiRouter implements Router {
+
+    public void showName() {
+        System.out.println("小米路由器");
+    }
+}

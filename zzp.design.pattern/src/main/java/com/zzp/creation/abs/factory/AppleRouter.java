161b05283741cdf24e3ee7779da99119ba26edb4
karyzeng <354697171@qq.com>
2019-11-21 10:37:31 +0800

    苹果路由器
    Signed-off-by: karyzeng <354697171@qq.com>

---
+++ zzp.design.pattern/src/main/java/com/zzp/creation/abs/factory/AppleRouter.java
@@ -0,0 +1,13 @@
+package com.zzp.creation.abs.factory;
+
+/**
+ * @Description 苹果路由器
+ * @Author Garyzeng
+ * @since 2019.11.21
+ **/
+public class AppleRouter implements Router {
+
+    public void showName() {
+        System.out.println("苹果路由器");
+    }
+}

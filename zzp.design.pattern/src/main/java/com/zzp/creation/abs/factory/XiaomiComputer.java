b572012228a26c2dfe99fad9b6233c4cfbe24a7f
karyzeng <354697171@qq.com>
2019-11-21 10:38:37 +0800

    小米电脑
    Signed-off-by: karyzeng <354697171@qq.com>

---
+++ zzp.design.pattern/src/main/java/com/zzp/creation/abs/factory/XiaomiComputer.java
@@ -0,0 +1,13 @@
+package com.zzp.creation.abs.factory;
+
+/**
+ * @Description 小米电脑
+ * @Author Garyzeng
+ * @since 2019.11.21
+ **/
+public class XiaomiComputer implements Computer {
+
+    public void showName() {
+        System.out.println("小米电脑");
+    }
+}

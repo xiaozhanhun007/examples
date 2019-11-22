a1a1714852a883c5f7020d69171d5dcabc2e8362
karyzeng <354697171@qq.com>
2019-11-22 16:39:22 +0800

    非超时缓存
    Signed-off-by: karyzeng <354697171@qq.com>

---
+++ zzp.util/src/main/java/com/util/caches/NormalCache.java
@@ -0,0 +1,23 @@
+package com.util.caches;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @Description 非超时缓存
+ * @Author Garyzeng
+ * @since 2019.11.22
+ **/
+public class NormalCache extends Cache<String, Object>{
+
+    private final AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    @Override
+    protected boolean cacheValid(Object value) {
+        return value != null;
+    }
+
+    @Override
+    protected Object getValue(String key) {
+        return key + "-" + atomicInteger.incrementAndGet();
+    }
+}

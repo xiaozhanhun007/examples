ab4eaf7e421dfca445402838c079f8cce9121c2e
zzp <354697171@qq.com>
2019-11-24 22:09:47 +0800

    狗接口

---
+++ zzp.design.pattern/src/main/java/com/zzp/structure/adapter/Dog.java
@@ -0,0 +1,20 @@
+package com.zzp.structure.adapter;
+
+/**
+ * @Description 狗接口
+ * @Author Garyzeng
+ * @since 2019.11.24
+ **/
+public interface Dog {
+
+    /**
+     * 狗叫声
+     */
+    void bark();
+
+    /**
+     * 跑
+     */
+    void run();
+
+}

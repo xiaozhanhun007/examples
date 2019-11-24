e32c405fba556fb8c9252b6dcbcb71b4fcc09562
zzp <354697171@qq.com>
2019-11-24 22:04:24 +0800

    代理模式接口

---
+++ zzp.design.pattern/src/main/java/com/zzp/structure/proxy/CodeService.java
@@ -0,0 +1,14 @@
+package com.zzp.structure.proxy;
+
+/**
+ * @Description 代理模式接口
+ * @Author Garyzeng
+ * @since 2019.11.24
+ **/
+public interface CodeService {
+
+    void codeJava();
+
+    void codePython();
+
+}

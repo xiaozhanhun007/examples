32a6c3a8b42ee14f0be4fdae2e0eb72bd3adc614
karyzeng <354697171@qq.com>
2019-11-25 17:48:53 +0800

    猫接口
    Signed-off-by: karyzeng <354697171@qq.com>

---
+++ zzp.design.pattern/src/main/java/com/zzp/structure/adapter/Cat.java
@@ -0,0 +1,20 @@
+package com.zzp.structure.adapter;
+
+/**
+ * @Description 猫接口
+ * @Author Garyzeng
+ * @since 2019.11.25
+ **/
+public interface Cat {
+
+    /**
+     * 猫叫声
+     */
+    void meow();
+
+    /**
+     * 跑
+     */
+    void run();
+
+}

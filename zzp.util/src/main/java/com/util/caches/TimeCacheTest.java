93d46f2b7611d6927a6a44fc1b153d29d8aa7ba2
karyzeng <354697171@qq.com>
2019-11-25 17:47:49 +0800

    修改TimeCacheTest
    Signed-off-by: karyzeng <354697171@qq.com>

--- zzp.util/src/main/java/com/util/caches/TimeCacheTest.java
+++ zzp.util/src/main/java/com/util/caches/TimeCacheTest.java
@@ -3,7 +3,10 @@ package com.util.caches;
 import com.alibaba.fastjson.JSON;
 import org.apache.commons.lang3.time.DateFormatUtils;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @Description 超时缓存测试
@@ -14,16 +17,28 @@ public class TimeCacheTest {
 
     public static void main(String[] args) {
 
-        Cache<String, TimeValue> cache = new TimeCache();
+        Cache<String, TimeValue> caches = new TimeCache();
+
+        final AtomicInteger atomicInteger = new AtomicInteger(0);
 
         Thread threadA = new Thread(new Runnable() {
             @Override
             public void run() {
                 for (int i = 0; i < 100; i++) {
                     String key = "TEST-A";
-                    TimeValue value = cache.get(key);
+                    TimeValue cache = caches.get(key);
+                    if (!caches.cacheValid(cache)) {
+                        // 缓存校验不通过，需要重新获取
+                        TimeValue timeValue = new TimeValue();
+                        List<String> list = new ArrayList<String>();
+                        list.add(key + "-" + atomicInteger.incrementAndGet());
+                        timeValue.setList(list);
+                        timeValue.setTimestamp(System.currentTimeMillis());
+                        caches.put(key, timeValue);
+                        cache = caches.get(key);
+                    }
                     String nowDateStr = DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss");
-                    System.out.println(nowDateStr + " -- " + Thread.currentThread().getName() + " -- key:" + key + ",value:" + JSON.toJSONString(value));
+                    System.out.println(nowDateStr + " -- " + Thread.currentThread().getName() + " -- key:" + key + ",value:" + JSON.toJSONString(cache) + ",cache.count:" + caches.getCount());
                     try {
                         Thread.sleep(6 * 1000);
                     } catch (InterruptedException e) {
@@ -38,9 +53,19 @@ public class TimeCacheTest {
             public void run() {
                 for (int i = 0; i < 100; i++) {
                     String key = "TEST-A";
-                    TimeValue value = cache.get(key);
+                    TimeValue cache = caches.get(key);
+                    if (!caches.cacheValid(cache)) {
+                        // 缓存校验不通过，需要重新获取
+                        TimeValue timeValue = new TimeValue();
+                        List<String> list = new ArrayList<String>();
+                        list.add(key + "-" + atomicInteger.incrementAndGet());
+                        timeValue.setList(list);
+                        timeValue.setTimestamp(System.currentTimeMillis());
+                        caches.put(key, timeValue);
+                        cache = caches.get(key);
+                    }
                     String nowDateStr = DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss");
-                    System.out.println(nowDateStr + " -- " + Thread.currentThread().getName() + " -- key:" + key + ",value:" + JSON.toJSONString(value));
+                    System.out.println(nowDateStr + " -- " + Thread.currentThread().getName() + " -- key:" + key + ",value:" + JSON.toJSONString(cache) + ",cache.count:" + caches.getCount());
                     try {
                         Thread.sleep(5 * 1000);
                     } catch (InterruptedException e) {

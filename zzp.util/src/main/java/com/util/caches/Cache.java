27f9b115900661ed512129d38feee50951e8db00
karyzeng <354697171@qq.com>
2019-11-25 17:47:16 +0800

    修改Cache
    Signed-off-by: karyzeng <354697171@qq.com>

--- zzp.util/src/main/java/com/util/caches/Cache.java
+++ zzp.util/src/main/java/com/util/caches/Cache.java
@@ -21,23 +21,49 @@ public abstract class Cache<K, V> {
 
     private final Lock writeLock = rwl.writeLock();
 
+    private long count = 0;
+
+    /**
+     * get值
+     * @param key
+     * @return
+     */
     public V get(K key){
         // 获取读锁
         readLock.lock();
-        V value = m.get(key);
-        if (!cacheValid(value)) {
+        try {
+            V value = m.get(key);
+            return value;
+        } finally {
+            // 释放读锁
+            readLock.unlock();
+        }
+    }
+
+    /**
+     * put值，会校验原来的值是否能通过校验
+     * 如果能通过校验则value不会替换oldValue
+     * 如果不能通过校验则value会替换oldValue
+     * @param key
+     * @param value
+     * @return 返回oldValue
+     */
+    public V put(K key, V value) {
+        // 获取读锁
+        readLock.lock();
+        V oldValue = m.get(key);
+        if (!cacheValid(oldValue)) {
             // 释放读锁，因为不允许读锁升级为写锁
             readLock.unlock();
             // 获取写锁
             writeLock.lock();
             try {
                 // 再次检查状态，防止多线程访问的时候，重复put值
-                value = m.get(key);
-                if (!cacheValid(value)) {
+                oldValue = m.get(key);
+                if (!cacheValid(oldValue)) {
                     // put值
-                    V newValue = getValue(key);
-                    m.put(key, newValue);
-                    value = newValue;
+                    m.put(key, value);
+                    count++;
                 }
                 // 释放写锁前，降级为读锁
                 readLock.lock();
@@ -49,7 +75,7 @@ public abstract class Cache<K, V> {
 
         // 此时还拥有读锁
         try {
-            return value;
+            return oldValue;
         } finally {
             // 释放读锁
             readLock.unlock();
@@ -63,11 +89,7 @@ public abstract class Cache<K, V> {
      */
     protected abstract boolean cacheValid(V value);
 
-    /**
-     * 获取Value，需要由子类实现
-     * @param key
-     * @return
-     */
-    protected abstract V getValue(K key);
-
+    public long getCount() {
+        return count;
+    }
 }

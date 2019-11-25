12d5c20f6c8fb4c6f96ae80ba0d633441cab8554
karyzeng <354697171@qq.com>
2019-11-25 17:47:32 +0800

    修改TimeCache
    Signed-off-by: karyzeng <354697171@qq.com>

--- zzp.util/src/main/java/com/util/caches/TimeCache.java
+++ zzp.util/src/main/java/com/util/caches/TimeCache.java
@@ -1,9 +1,5 @@
 package com.util.caches;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-
 /**
  * @Description 超时缓存
  * @Author Garyzeng
@@ -12,8 +8,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class TimeCache extends Cache<String, TimeValue> {
 
     private final long effectiveTime = 30 * 1000;// 缓存有效时间长度，单位毫秒
-
-    private final AtomicInteger atomicInteger = new AtomicInteger(0);
 
     @Override
     protected boolean cacheValid(TimeValue value) {
@@ -24,13 +18,4 @@ public class TimeCache extends Cache<String, TimeValue> {
         return false;
     }
 
-    @Override
-    protected TimeValue getValue(String key) {
-        TimeValue timeValue = new TimeValue();
-        List<String> list = new ArrayList<String>();
-        list.add(key + "-" + atomicInteger.incrementAndGet());
-        timeValue.setList(list);
-        timeValue.setTimestamp(System.currentTimeMillis());
-        return timeValue;
-    }
 }

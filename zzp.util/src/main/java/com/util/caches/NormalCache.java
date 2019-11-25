cd856745f7107c334f1192e15a7f90f763374c2f
zzp <354697171@qq.com>
2019-11-25 21:37:05 +0800

    修改NormalCache

--- zzp.util/src/main/java/com/util/caches/NormalCache.java
+++ zzp.util/src/main/java/com/util/caches/NormalCache.java
@@ -1,7 +1,5 @@
 package com.util.caches;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 /**
  * @Description 非超时缓存
  * @Author Garyzeng
@@ -9,15 +7,10 @@ import java.util.concurrent.atomic.AtomicInteger;
  **/
 public class NormalCache extends Cache<String, Object>{
 
-//    private final AtomicInteger atomicInteger = new AtomicInteger(0);
 
     @Override
     protected boolean cacheValid(Object value) {
         return value != null;
     }
 
-//    @Override
-//    protected Object getValue(String key) {
-//        return key + "-" + atomicInteger.incrementAndGet();
-//    }
 }

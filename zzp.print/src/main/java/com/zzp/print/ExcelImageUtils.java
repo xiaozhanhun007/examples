eb0bdfa7c45cf7d426196f36ca9240532db8d91b
karyzeng <354697171@qq.com>
2019-11-29 09:09:52 +0800

    添加batchInsertImageToExcel方法
    Signed-off-by: karyzeng <354697171@qq.com>

--- zzp.print/src/main/java/com/zzp/print/ExcelImageUtils.java
+++ zzp.print/src/main/java/com/zzp/print/ExcelImageUtils.java
@@ -10,8 +10,10 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.util.List;
 
 /**
  * @Description excel插入图片工具
@@ -22,6 +24,44 @@ public class ExcelImageUtils {
 
     public static void insertImageToExcel(String excelFilePath, String imgPath, String imgType) throws Exception{
         insertImageToExcel(excelFilePath, imgPath, imgType, 0, 0, 0, 0, (short) 38, 0, (short) 51, 1);
+    }
+
+    /**
+     * 批量将图片插入到对应的excel文件中
+     * @param excelFileFolderPath
+     * @param imgFolderPath
+     * @param imgType
+     * @param dx1
+     * @param dy1
+     * @param dx2
+     * @param dy2
+     * @param col1
+     * @param row1
+     * @param col2
+     * @param row2
+     * @throws Exception
+     */
+    public static void batchInsertImageToExcel(String excelFileFolderPath, String imgFolderPath, String imgType, int dx1, int dy1, int dx2, int dy2, short col1, int row1, short col2, int row2) throws Exception{
+        File excelFileFolder = new File(excelFileFolderPath);
+        if (!excelFileFolder.exists()) {
+            throw new FileNotFoundException("找不到excelFileFolder文件夹");
+        }
+        File imgFolder = new File(imgFolderPath);
+        if (!imgFolder.exists()) {
+            throw new FileNotFoundException("找不到imgFolder文件夹");
+        }
+        List<String> imgNames = FileUtils.getFilePaths(imgFolder);
+        if (imgNames == null || imgNames.size() == 0) {
+            throw new FileNotFoundException("imgFolder文件夹不存在图片");
+        }
+        for (int i = 0; i < imgNames.size(); i++) {
+            String imgName = imgNames.get(i);
+            String imgNamePrefix = imgName.substring(0, imgName.lastIndexOf("."));
+            String excelName = imgNamePrefix + ".xls";
+            String imgPath = imgFolderPath + File.separator + imgName;
+            String excelFilePath = excelFileFolderPath + File.separator + excelName;
+            insertImageToExcel(excelFilePath, imgPath, imgType, dx1, dy1, dx2, dy2, col1, row1, col2, row2);
+        }
     }
 
     /**
@@ -40,6 +80,14 @@ public class ExcelImageUtils {
      * @throws Exception
      */
     public static void insertImageToExcel(String excelFilePath, String imgPath, String imgType, int dx1, int dy1, int dx2, int dy2, short col1, int row1, short col2, int row2) throws Exception{
+        File excelFile = new File(excelFilePath);
+        if (!excelFile.exists()) {
+            throw new FileNotFoundException("找不到excelFile文件");
+        }
+        File imgFile = new File(imgPath);
+        if (!imgFile.exists()) {
+            throw new FileNotFoundException("找不到img文件");
+        }
         int PICTURE_TYPE = 0;
         if (imgType.equals("png")) {
             PICTURE_TYPE = HSSFWorkbook.PICTURE_TYPE_PNG;

e5afaa9d501f83a82ffc6db4e2e149d893767562
karyzeng <354697171@qq.com>
2019-12-02 08:56:29 +0800

    将com.zzp.controller修改为com.zzp.provider.controller
    Signed-off-by: karyzeng <354697171@qq.com>

--- spring.activemq.provider/src/main/java/com/zzp/provider/controller/ProviderController.java
+++ spring.activemq.provider/src/main/java/com/zzp/provider/controller/ProviderController.java
@@ -1,4 +1,4 @@
-package com.zzp.controller;
+package com.zzp.provider.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;

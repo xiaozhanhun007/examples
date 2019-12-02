fcc481a86674364e833541bbe483695360793924
karyzeng <354697171@qq.com>
2019-12-02 17:48:48 +0800

    添加SpringBootApplication
    Signed-off-by: karyzeng <354697171@qq.com>

--- spring.activemq.provider/src/main/java/com/zzp/Application.java
+++ spring.activemq.provider/src/main/java/com/zzp/Application.java
@@ -1,20 +1,16 @@
 package com.zzp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 启动类
  * @author karyzeng 2018.07.10
  * @version 1.0
  */
-@Configuration  
-@EnableAutoConfiguration  
-@ComponentScan
-@EnableScheduling
+@SpringBootApplication
+@ComponentScan(basePackages={"com.zzp"})
 public class Application {
 
 	public static void main(String[] args) {

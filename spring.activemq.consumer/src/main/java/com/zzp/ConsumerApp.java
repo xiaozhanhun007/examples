f7124d223b8474e16976e49a9e002ab1ba634b40
karyzeng <354697171@qq.com>
2019-12-03 17:36:41 +0800

    添加activemq配置
    Signed-off-by: karyzeng <354697171@qq.com>

--- spring.activemq.consumer/src/main/java/com/zzp/ConsumerApp.java
+++ spring.activemq.consumer/src/main/java/com/zzp/ConsumerApp.java
@@ -1,11 +1,19 @@
 package com.zzp;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.jms.ConnectionFactory;
 
 /**
  * @Description 启动类
@@ -20,5 +28,28 @@ public class ConsumerApp {
 
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApp.class, args);
+    }
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+        connectionFactory.setBrokerURL(env.getProperty("spring.activemq.broker-url"));
+        connectionFactory.setUserName(env.getProperty("spring.activemq.user"));
+        connectionFactory.setPassword(env.getProperty("spring.activemq.password"));
+        return connectionFactory;
+    }
+
+    @Bean
+    public JmsTemplate genJmsTemplate() {
+        return new JmsTemplate(connectionFactory());
+
+    }
+
+    @Bean
+    public JmsMessagingTemplate jmsMessageTemplate() {
+        return new JmsMessagingTemplate(connectionFactory());
     }
 }

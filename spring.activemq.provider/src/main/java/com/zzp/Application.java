5ed06c7d7ac359b3f6c809fd4289e0a360735257
karyzeng <354697171@qq.com>
2019-12-03 17:31:57 +0800

    添加activemq配置
    Signed-off-by: karyzeng <354697171@qq.com>

--- spring.activemq.provider/src/main/java/com/zzp/Application.java
+++ spring.activemq.provider/src/main/java/com/zzp/Application.java
@@ -1,11 +1,19 @@
 package com.zzp;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import javax.jms.ConnectionFactory;
 
 /**
  * 启动类
@@ -16,10 +24,34 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @ComponentScan(basePackages={"com.zzp"})
 @EnableScheduling
 @MapperScan("com.zzp.**.mapper")
+@EnableJms
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);  
 	}
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+        connectionFactory.setBrokerURL(env.getProperty("spring.activemq.broker-url"));
+        connectionFactory.setUserName(env.getProperty("spring.activemq.user"));
+        connectionFactory.setPassword(env.getProperty("spring.activemq.password"));
+        return connectionFactory;
+    }
+
+    @Bean
+    public JmsTemplate genJmsTemplate() {
+        return new JmsTemplate(connectionFactory());
+
+    }
+
+    @Bean
+    public JmsMessagingTemplate jmsMessageTemplate() {
+        return new JmsMessagingTemplate(connectionFactory());
+    }
 
 }

696ee8845b55127190ad0039556b746262223873
karyzeng <354697171@qq.com>
2019-12-04 17:39:40 +0800

    添加@PropertySource
    Signed-off-by: karyzeng <354697171@qq.com>

--- spring.activemq.provider/src/main/java/com/zzp/Application.java
+++ spring.activemq.provider/src/main/java/com/zzp/Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.core.JmsMessagingTemplate;
@@ -25,6 +26,7 @@ import javax.jms.ConnectionFactory;
 @EnableScheduling
 @MapperScan("com.zzp.**.mapper")
 @EnableJms
+@PropertySource(value={"classpath:constants.properties"})
 public class Application {
 
 	public static void main(String[] args) {

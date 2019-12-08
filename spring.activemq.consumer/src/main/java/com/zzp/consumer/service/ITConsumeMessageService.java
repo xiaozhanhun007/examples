5b6377fc51ff4b4a71e61985be20b02e8463add9
zzp <354697171@qq.com>
2019-12-08 21:56:31 +0800

    已经消费的消息 服务类

---
+++ spring.activemq.consumer/src/main/java/com/zzp/consumer/service/ITConsumeMessageService.java
@@ -0,0 +1,16 @@
+package com.zzp.consumer.service;
+
+import com.zzp.consumer.entity.TConsumeMessage;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 已经消费的消息 服务类
+ * </p>
+ *
+ * @author zzp
+ * @since 2019-12-08
+ */
+public interface ITConsumeMessageService extends IService<TConsumeMessage> {
+
+}

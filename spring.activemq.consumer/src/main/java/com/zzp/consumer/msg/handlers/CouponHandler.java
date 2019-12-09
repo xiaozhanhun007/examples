92be31a2343717ecfe7c9baeaa94eeae1745c150
karyzeng <354697171@qq.com>
2019-12-09 17:32:55 +0800

    修改handler方法
    Signed-off-by: karyzeng <354697171@qq.com>

--- spring.activemq.consumer/src/main/java/com/zzp/consumer/msg/handlers/CouponHandler.java
+++ spring.activemq.consumer/src/main/java/com/zzp/consumer/msg/handlers/CouponHandler.java
@@ -2,8 +2,15 @@ package com.zzp.consumer.msg.handlers;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.zzp.base.utils.SpringUtil;
+import com.zzp.consumer.entity.TConsumeMessage;
 import com.zzp.consumer.entity.TCoupon;
+import com.zzp.consumer.service.ITConsumeMessageService;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
 
 /**
  * @Description 优惠券handler
@@ -11,6 +18,10 @@ import org.apache.commons.lang3.StringUtils;
  * @since 2019.12.07
  **/
 public class CouponHandler implements BaseHandler {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private ITConsumeMessageService consumeMessageService = SpringUtil.getBean(ITConsumeMessageService.class);
 
     public boolean hasHandle(String msg) {
         JSONObject msgJson = JSON.parseObject(msg);
@@ -24,9 +35,25 @@ public class CouponHandler implements BaseHandler {
     public void handler(String msg) {
         if (this.hasHandle(msg)) {
             JSONObject msgJson = JSON.parseObject(msg);
-            JSONObject dataJson = msgJson.getJSONObject("data");
-            TCoupon coupon = JSON.parseObject(dataJson.toJSONString(), TCoupon.class);
-            System.out.println(coupon.getCouponName());
+            String type = msgJson.getString("type");
+            String msgId = msgJson.getString("msgId");
+            TConsumeMessage consumeMessage = consumeMessageService.getConsumeMessage(msgId);
+            if (consumeMessage == null) {
+                // 表示还未消费过该消息
+                JSONObject dataJson = msgJson.getJSONObject("data");
+                TCoupon coupon = JSON.parseObject(dataJson.toJSONString(), TCoupon.class);
+                logger.info(coupon.getCouponName());
+                TConsumeMessage message = new TConsumeMessage();
+                message.setMsgId(msgId);
+                message.setType(type);
+                message.setContent(msg);
+                message.setCreateTime(new Date());
+                consumeMessageService.save(message);
+            } else {
+                // 表示已消费过该消息
+                logger.info("已消费过该消息，msgId：" + msgId + "，msg：" + msg);
+            }
+
         }
     }
 }

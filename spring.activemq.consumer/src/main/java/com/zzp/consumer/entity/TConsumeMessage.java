6a0addc48faf7dd16de3aaa49f513ff87688e9c4
karyzeng <354697171@qq.com>
2019-12-09 17:31:38 +0800

    添加createTime属性
    Signed-off-by: karyzeng <354697171@qq.com>

--- spring.activemq.consumer/src/main/java/com/zzp/consumer/entity/TConsumeMessage.java
+++ spring.activemq.consumer/src/main/java/com/zzp/consumer/entity/TConsumeMessage.java
@@ -1,6 +1,8 @@
 package com.zzp.consumer.entity;
 
 import java.io.Serializable;
+import java.util.Date;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -39,6 +41,11 @@ public class TConsumeMessage implements Serializable {
      * 消息内容
      */
     private String content;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
 
 
 }

d305964e578b352d0c76e88b27ab585dbcacd63c
karyzeng <354697171@qq.com>
2019-12-12 18:40:14 +0800

    添加updateAllColumnById方法
    Signed-off-by: karyzeng <354697171@qq.com>

--- spring.activemq.provider/src/main/java/com/zzp/provider/service/impl/TSysUserServiceImpl.java
+++ spring.activemq.provider/src/main/java/com/zzp/provider/service/impl/TSysUserServiceImpl.java
@@ -4,6 +4,7 @@ import com.zzp.provider.entity.TSysUser;
 import com.zzp.provider.mapper.TSysUserMapper;
 import com.zzp.provider.service.ITSysUserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,4 +18,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class TSysUserServiceImpl extends ServiceImpl<TSysUserMapper, TSysUser> implements ITSysUserService {
 
+    @Autowired
+    private TSysUserMapper sysUserMapper;
+
+    @Override
+    public void updateAllColumnById(TSysUser entity) {
+        sysUserMapper.updateAllColumnById(entity);
+    }
 }

f8dd81ada175b323cab994042f51e0bad4135e25
zzp <354697171@qq.com>
2019-12-13 06:14:28 +0800

    修改injectMappedStatement方法

--- spring.boot.base/src/main/java/com/zzp/base/method/UpdateAllColumnById.java
+++ spring.boot.base/src/main/java/com/zzp/base/method/UpdateAllColumnById.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.core.injector.AbstractMethod;
 import com.baomidou.mybatisplus.core.metadata.TableFieldInfo;
 import com.baomidou.mybatisplus.core.metadata.TableInfo;
 import com.baomidou.mybatisplus.core.toolkit.Constants;
+import com.zzp.base.enums.CustomSqlMethod;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.mapping.SqlSource;
 
@@ -20,7 +21,7 @@ public class UpdateAllColumnById extends AbstractMethod {
 
     @Override
     public MappedStatement injectMappedStatement(final Class<?> mapperClass, final Class<?> modelClass, final TableInfo tableInfo) {
-//        final SqlMethod sqlMethod = SqlMethod.UPDATE_BY_ID;
+        final CustomSqlMethod sqlMethod = CustomSqlMethod.LOGIC_UPDATE_ALL_COLUMN_BY_ID;
 
         // 反射修改fieldFill值为update
         final List<TableFieldInfo> fieldList = tableInfo.getFieldList();
@@ -37,7 +38,7 @@ public class UpdateAllColumnById extends AbstractMethod {
             }
         }
 
-        final String sql = String.format("<script>\nUPDATE %s %s WHERE %s=#{%s} %s\n</script>", tableInfo.getTableName(),
+        final String sql = String.format(sqlMethod.getSql(), tableInfo.getTableName(),
                 this.sqlSet(false, false, tableInfo, false, null, Constants.ENTITY_DOT),
                 tableInfo.getKeyColumn(), Constants.ENTITY_DOT + tableInfo.getKeyProperty(),
                 new StringBuilder("<if test=\"et instanceof java.util.Map\">")
@@ -45,6 +46,6 @@ public class UpdateAllColumnById extends AbstractMethod {
                         .append(" AND ${et.MP_OPTLOCK_VERSION_COLUMN}=#{et.MP_OPTLOCK_VERSION_ORIGINAL}")
                         .append("</if></if>"));
         final SqlSource sqlSource = this.languageDriver.createSqlSource(this.configuration, sql, modelClass);
-        return this.addUpdateMappedStatement(mapperClass, modelClass, "updateAllColumnById", sqlSource);
+        return this.addUpdateMappedStatement(mapperClass, modelClass, sqlMethod.getMethod(), sqlSource);
     }
 }

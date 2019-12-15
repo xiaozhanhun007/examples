7941422481e17e2e5928e62e9240223ecd7c7cb6
karyzeng <354697171@qq.com>
2019-12-15 15:21:17 +0800

    修改run方法
    Signed-off-by: karyzeng <354697171@qq.com>

--- spring.activemq.provider/src/main/java/com/zzp/provider/timing/tasks/SendMessageTask.java
+++ spring.activemq.provider/src/main/java/com/zzp/provider/timing/tasks/SendMessageTask.java
@@ -1,9 +1,17 @@
 package com.zzp.provider.timing.tasks;
 
+import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.zzp.base.enums.CommonJudgeEnum;
+import com.zzp.provider.entity.TSendMessage;
+import com.zzp.provider.service.ITSendMessageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * @Description 发送消息到mq的定时任务
@@ -15,9 +23,27 @@ public class SendMessageTask {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Scheduled(cron = "0 0/1 * * * *")
+    @Autowired
+    private ITSendMessageService sendMessageService;
+
+    @Scheduled(cron = "0 0/2 * * * *")
     public void run() {
         logger.info("发送消息到mq任务开始");
+
+        QueryWrapper<TSendMessage> queryWrapper = new QueryWrapper<TSendMessage>();
+        queryWrapper.eq("send_flag", CommonJudgeEnum.NO.getId());
+        queryWrapper.orderByAsc("create_time");
+        List<TSendMessage> sendMessages = sendMessageService.list(queryWrapper);
+        if (sendMessages != null && sendMessages.size() > 0) {
+            for (int i = 0; i < sendMessages.size(); i++) {
+                TSendMessage sendMessage = sendMessages.get(i);
+                sendMessageService.sendMqAndUpdateSendFlag(sendMessage);
+                logger.info("发送消息到mq任务，msgId：" + sendMessage.getMsgId() + "，sendMessage：" + JSON.toJSONString(sendMessage) + "，发送成功");
+            }
+        } else {
+            logger.info("发送消息到mq任务，sendMessages列表为空");
+        }
+
         logger.info("发送消息到mq任务结束");
     }
 

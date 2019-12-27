c24e15973f56e82f566e95bf4b8586583ae6baf5
karyzeng <354697171@qq.com>
2019-12-27 17:05:53 +0800

    修改HttpRequest
    Signed-off-by: karyzeng <354697171@qq.com>

--- zzp.util/src/main/java/com/util/http/HttpRequest.java
+++ zzp.util/src/main/java/com/util/http/HttpRequest.java
@@ -24,7 +24,7 @@ import com.util.sort.MapSort;
 public class HttpRequest {
 
 	public static void main(String[] args) {
-		System.out.println(HttpRequest.sendGet("http://api.map.baidu.com/geocoder/v2/", "address=  广东省  广州市天河区华景  	新城东区华景二期粤生街98号405 &output=json&ak=KaqwkA7ozlPx4KgYVXbTfbGOGUXiIp3R"));
+//		System.out.println(HttpRequest.sendGet("http://api.map.baidu.com/geocoder/v2/", "address=  广东省  广州市天河区华景  	新城东区华景二期粤生街98号405 &output=json&ak=KaqwkA7ozlPx4KgYVXbTfbGOGUXiIp3R"));
 		
 //		Map<String, String> params = new HashMap<String, String>();
 //		params.put("method", "GetVehicleNoList");
@@ -32,24 +32,90 @@ public class HttpRequest {
 //		params.put("timestamp", "2018-08-29 16:35:36");
 //		params.put("format", "json");
 //		System.out.println(HttpRequest.sendGet("http://api.e6gps.com/public/v3/StatisticsReport/Call", "69411cff-d7be-4458-952f-2fca0267b408", params));
+
 	}
 	
-	/**
+    /**
      * 向指定URL发送GET方法的请求
      * 
      * @param url 发送请求的URL
-     * @param param 请求参数，请求参数应该是 name1=value1&name2=value2 的形式
+     * @param appsecret 私钥
+     * @param params 签名参数自动生成，不需要传入
      * 
      * @return String 响应结果
      */
-    public static String sendGet(String url, String param) {
-    	//去除参数中可能含有的空格
-    	param = ConvertUtil.trimSpace(param);
+    public static String sendGetByMD5(String url, String appsecret, Map<String, String> params){
+    	String sign = getSignByMD5(appsecret, params);
+    	params.put("sign", sign);
+        String param = convertParamList2ParamStr(params);
+    	return sendGet(url, param);
+    }
+    
+    /**
+     * 向指定URL发送GET方法的请求
+     * 
+     * @param url 发送请求的URL
+     * @param appsecret 私钥
+     * @param params 签名参数自动生成，不需要传入
+     * 
+     * @return String 响应结果
+     */
+    public static String sendGetBySHA1(String url, String appsecret, Map<String, String> params){
+    	String appId = params.get("appId");
+    	String time = params.get("time");
+    	String signature = getSignBySHA1(appId, appsecret, time);
+    	params.put("signature", signature);
+        String param = convertParamList2ParamStr(params);
+    	return sendGet(url, param);
+    }
+
+    /**
+     * get请求
+     * @param url 请求url
+     * @param params 参数map
+     * @return String
+     */
+    public static String sendGet(String url, Map<String, String> params){
+        String param = convertParamList2ParamStr(params);
+        return sendGet(url, param, null);
+    }
+
+    /**
+     * get请求
+     * @param url 请求url
+     * @param params 参数map
+     * @param requestPropertys 请求属性
+     * @return String
+     */
+    public static String sendGet(String url, Map<String, String> params, Map<String, String> requestPropertys){
+        String param = convertParamList2ParamStr(params);
+        return sendGet(url, param, requestPropertys);
+    }
+
+    /**
+     * get请求
+     * @param url 请求url
+     * @param param 请求参数，格式如name1=value1&name2=value2
+     * @return String
+     */
+    public static String sendGet(String url, String param){
+        return sendGet(url, param, null);
+    }
+
+    /**
+     * get请求
+     * @param url 请求url
+     * @param param 请求参数，格式如name1=value1&name2=value2
+     * @return String
+     */
+    public static String sendGet(String url, String param, Map<String, String> requestPropertys) {
+        //去除参数中可能含有的空格
+        param = ConvertUtil.trimSpace(param);
         String result = "";
         BufferedReader in = null;
         try {
             String urlNameString = url + "?" + param;
-            System.out.println(urlNameString);
+            System.out.println("请求完整的url：" + urlNameString);
             URL realUrl = new URL(urlNameString);
             // 打开和URL之间的连接
             URLConnection connection = realUrl.openConnection();
@@ -57,6 +123,11 @@ public class HttpRequest {
             connection.setRequestProperty("accept", "*/*");
             connection.setRequestProperty("connection", "Keep-Alive");
             connection.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            if (requestPropertys != null && requestPropertys.size() > 0) {
+                for (Map.Entry<String, String> entry : requestPropertys.entrySet()) {
+                    connection.setRequestProperty(entry.getKey(), entry.getValue());
+                }
+            }
             //设置连接超时时间，必须设置，否则会让程序卡在请求连接
             connection.setConnectTimeout(15000);
             //设置读超时时间，必须设置，否则会让程序卡在读内容中
@@ -93,109 +164,43 @@ public class HttpRequest {
     }
     
     /**
-     * 向指定URL发送GET方法的请求
-     * 
-     * @param url 发送请求的URL
-     * @param appsecret 私钥
-     * @param params 签名参数自动生成，不需要传入
-     * 
-     * @return String 响应结果
-     */
-    public static String sendGetByMD5(String url, String appsecret, Map<String, String> params){
-    	String sign = getSignByMD5(appsecret, params);
-    	StringBuffer sBuffer = new StringBuffer();
-    	for (Map.Entry<String, String> entry : params.entrySet()) {
-    		String value = "";
-			try {
-				value = URLEncoder.encode(entry.getValue(),"utf-8");
-			} catch (UnsupportedEncodingException e) {
-				e.printStackTrace();
-			}
-			sBuffer.append(entry.getKey() + "=" + value + "&");
-    	}
-    	sBuffer.append("sign=" +sign);
-    	return sendGet(url, sBuffer.toString());
-    }
-    
-    /**
-     * 获得MD5加密签名sign
-     * 
-     * @param appsecret 私钥
-     * @param params 签名参数sign自动生成，不需要传入
-     * 
-     * @return String 签名sign
-     */
-    public static String getSignByMD5(String appsecret, Map<String, String> params) {
-    	Map<String, String> sortMap = MapSort.sortMapByKey(params);
-    	StringBuffer sBuffer = new StringBuffer();
-    	sBuffer.append(appsecret);
-    	for (Map.Entry<String, String> entry : sortMap.entrySet()) {
-    		sBuffer.append(entry.getKey() + entry.getValue());
-    	}
-    	sBuffer.append(appsecret);
-    	return MD5Util.string2MD5(sBuffer.toString()).toUpperCase();
-    }
-    
-    /**
-     * 向指定URL发送GET方法的请求
-     * 
-     * @param url 发送请求的URL
-     * @param appsecret 私钥
-     * @param params 签名参数自动生成，不需要传入
-     * 
-     * @return String 响应结果
-     */
-    public static String sendGetBySHA1(String url, String appsecret, Map<String, String> params){
-    	String appId = params.get("appId");
-    	String time = params.get("time");
-    	String signature = getSignBySHA1(appId, appsecret, time);
-    	StringBuffer sBuffer = new StringBuffer();
-    	for (Map.Entry<String, String> entry : params.entrySet()) {
-    		String value = "";
-			try {
-				value = URLEncoder.encode(entry.getValue(),"utf-8");
-			} catch (UnsupportedEncodingException e) {
-				e.printStackTrace();
-			}
-			sBuffer.append(entry.getKey() + "=" + value + "&");
-    	}
-    	sBuffer.append("signature=" + signature);
-    	return sendGet(url, sBuffer.toString());
-    }
-    
-    /**
-     * 获得SHA1加密签名sign
-     * 
-     * @param appId 
-     * @param appSecret 私钥
-     * @param time 当前时间字符串，格式“2018-10-13 11:02:10”
-     * @return
-     */
-    public static String getSignBySHA1(String appId, String appSecret, String time) {
-    	String sign = SHA1SignUtil.createSignature(appId, appSecret, time);
-    	return sign;
-    }
-    
-    /**
-     * 向指定 URL 发送POST方法的请求
-     * 
-     * @param url 发送请求的 URL
-     * @param param 请求参数，请求参数应该是 name1=value1&name2=value2 的形式
-     * 
-     * @return String 响应结果
+     * post请求
+     * @param url 请求url
+     * @param param 请求参数，格式如name1=value1&name2=value2
+     * @return String
      */
     public static String sendPost(String url, String param) {
         return sendPost(url, param, null);
     }
 
     /**
-     * 向指定 URL 发送POST方法的请求
-     *
-     * @param url 发送请求的 URL
-     * @param param 请求参数，请求参数应该是 name1=value1&name2=value2 的形式
+     * post请求
+     * @param url 请求url
+     * @param params 参数map
+     * @return String
+     */
+    public static String sendPost(String url, Map<String, String> params) {
+        String param = convertParamList2ParamStr(params);
+        return sendPost(url, param, null);
+    }
+
+    /**
+     * post请求
+     * @param url 请求url
+     * @param params 参数map
+     * @return String
+     */
+    public static String sendPost(String url, Map<String, String> params, Map<String, String> requestPropertys){
+        String param = convertParamList2ParamStr(params);
+        return sendPost(url, param, requestPropertys);
+    }
+
+    /**
+     * post请求
+     * @param url 请求url
+     * @param param 请求参数，格式如name1=value1&name2=value2
      * @param requestPropertys 请求属性
-     *
-     * @return String 响应结果
+     * @return String
      */
     public static String sendPost(String url, String param, Map<String, String> requestPropertys) {
         //去除参数中可能含有的空格
@@ -229,6 +234,12 @@ public class HttpRequest {
             out.print(param);
             // flush输出流的缓冲
             out.flush();
+            // 获取所有响应头字段
+            Map<String, List<String>> map = conn.getHeaderFields();
+            // 遍历所有的响应头字段
+            for (String key : map.keySet()) {
+                System.out.println(key + "--->" + map.get(key));
+            }
             // 定义BufferedReader输入流来读取URL的响应
             in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
             String line;
@@ -254,6 +265,58 @@ public class HttpRequest {
             }
         }
         return result;
+    }
+
+    /**
+     * 将参数Map转换成参数字符串，格式如name1=value1&name2=value2
+     * @param params 参数map
+     * @return String
+     */
+    private static String convertParamList2ParamStr(Map<String, String> params) {
+        if (params == null || params.size() == 0) {
+            return null;
+        }
+        StringBuffer sBuffer = new StringBuffer();
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            String value = "";
+            try {
+                value = URLEncoder.encode(entry.getValue(),"utf-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            sBuffer.append(entry.getKey() + "=" + value + "&");
+        }
+        sBuffer.deleteCharAt(sBuffer.lastIndexOf("&"));
+        return sBuffer.toString();
+    }
+
+    /**
+     * 获得MD5加密签名sign
+     * @param appsecret 私钥
+     * @param params 签名参数sign自动生成，不需要传入
+     * @return String 签名sign
+     */
+    public static String getSignByMD5(String appsecret, Map<String, String> params) {
+        Map<String, String> sortMap = MapSort.sortMapByKey(params);
+        StringBuffer sBuffer = new StringBuffer();
+        sBuffer.append(appsecret);
+        for (Map.Entry<String, String> entry : sortMap.entrySet()) {
+            sBuffer.append(entry.getKey() + entry.getValue());
+        }
+        sBuffer.append(appsecret);
+        return MD5Util.string2MD5(sBuffer.toString()).toUpperCase();
+    }
+
+    /**
+     * 获得SHA1加密签名sign
+     * @param appId
+     * @param appSecret 私钥
+     * @param time 当前时间字符串，格式“2018-10-13 11:02:10”
+     * @return
+     */
+    public static String getSignBySHA1(String appId, String appSecret, String time) {
+        String sign = SHA1SignUtil.createSignature(appId, appSecret, time);
+        return sign;
     }
 
 }

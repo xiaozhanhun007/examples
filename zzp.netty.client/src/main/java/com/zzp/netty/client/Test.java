72c00d1a6d2ae105372737d5d6b62b911f53e122
karyzeng <354697171@qq.com>
2020-01-02 17:38:21 +0800

    修改netty客户端测试
    Signed-off-by: karyzeng <354697171@qq.com>

--- zzp.netty.client/src/main/java/com/zzp/netty/client/Test.java
+++ zzp.netty.client/src/main/java/com/zzp/netty/client/Test.java
@@ -1,5 +1,10 @@
 package com.zzp.netty.client;
 
+import com.alibaba.fastjson.JSON;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Description netty客户端测试
  * @Author Garyzeng
@@ -10,7 +15,25 @@ public class Test {
     public static void main(String[] args) {
         NettyClient client = new NettyClient("127.0.0.1", 8080);
         client.run();
-        client.send("aaa");
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("id", 1);
+        map.put("name", "金甲卡卡龙");
+
+        String msg = JSON.toJSONString(map);
+
+        client.send(msg);
+
+        map.put("id", 2);
+        map.put("name", "保温杯");
+
+        client.send(JSON.toJSONString(map));
+
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        client.close();
     }
 
 }

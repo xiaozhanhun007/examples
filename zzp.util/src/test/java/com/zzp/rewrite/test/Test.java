3d2e929c3400836ceb9e0df616d470abe08514d5
zzp <354697171@qq.com>
2020-05-22 11:31:56 +0800

    测试方法重载

---
+++ zzp.util/src/test/java/com/zzp/rewrite/test/Test.java
@@ -0,0 +1,15 @@
+package com.zzp.rewrite.test;
+
+/**
+ * @Description 测试方法重载
+ * @Author Karyzeng
+ * @since 2020.05.22
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        VipCustomer vipCustomer = new VipCustomer();
+        vipCustomer.vipDiscount(16);
+    }
+
+}

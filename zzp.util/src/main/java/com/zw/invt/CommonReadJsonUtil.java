9e7689b2723e7c3824988f0eeb7fafae2d48de2a
karyzeng <354697171@qq.com>
2020-06-05 16:19:39 +0800

    读取json文件公共类
    Signed-off-by: karyzeng <354697171@qq.com>

---
+++ zzp.util/src/main/java/com/zw/invt/CommonReadJsonUtil.java
@@ -0,0 +1,39 @@
+package com.zw.invt;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+/**
+ * @Description 读取json文件公共类
+ * @Author karyzeng
+ * @since 2020.06.05
+ **/
+public class CommonReadJsonUtil {
+
+    //读取json文件
+    public static String readJsonFile(String fileName) {
+        String jsonStr = "";
+        try {
+            File jsonFile = new File(fileName);
+            FileReader fileReader = new FileReader(jsonFile);
+            Reader reader = new InputStreamReader(new FileInputStream(jsonFile),"utf-8");
+            int ch = 0;
+            StringBuffer sb = new StringBuffer();
+            while ((ch = reader.read()) != -1) {
+                sb.append((char) ch);
+            }
+            fileReader.close();
+            reader.close();
+            jsonStr = sb.toString();
+            return jsonStr;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}

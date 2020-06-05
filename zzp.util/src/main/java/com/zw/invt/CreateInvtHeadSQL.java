9e1c31505bd09fc75d0ebc6b2088f59699eb05fd
karyzeng <354697171@qq.com>
2020-06-05 16:20:36 +0800

    修改CreateInvtHeadSQL
    Signed-off-by: karyzeng <354697171@qq.com>

--- zzp.util/src/main/java/com/zw/invt/CreateInvtHeadSQL.java
+++ zzp.util/src/main/java/com/zw/invt/CreateInvtHeadSQL.java
@@ -3,13 +3,6 @@ package com.zw.invt;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,7 +27,7 @@ public class CreateInvtHeadSQL {
     public static List<InvtHead> init() {
         List<InvtHead> invtHeads = new ArrayList<InvtHead>();
         String path = CreateInvtHeadSQL.class.getClassLoader().getResource("invtHead.json").getPath();
-        String s = readJsonFile(path);
+        String s = CommonReadJsonUtil.readJsonFile(path);
         JSONArray jsonArray = JSON.parseArray(s);
         for (int i = 0; i < jsonArray.size(); i++) {
             JSONObject jsonObject = jsonArray.getJSONObject(i);
@@ -42,28 +35,6 @@ public class CreateInvtHeadSQL {
             invtHeads.add(invtHead);
         }
         return invtHeads;
-    }
-
-    //读取json文件
-    public static String readJsonFile(String fileName) {
-        String jsonStr = "";
-        try {
-            File jsonFile = new File(fileName);
-            FileReader fileReader = new FileReader(jsonFile);
-            Reader reader = new InputStreamReader(new FileInputStream(jsonFile),"utf-8");
-            int ch = 0;
-            StringBuffer sb = new StringBuffer();
-            while ((ch = reader.read()) != -1) {
-                sb.append((char) ch);
-            }
-            fileReader.close();
-            reader.close();
-            jsonStr = sb.toString();
-            return jsonStr;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
     }
 
 }

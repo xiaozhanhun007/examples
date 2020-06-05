88494a2c44272573052c241cd67930ae7b97ea08
karyzeng <354697171@qq.com>
2020-06-05 16:51:38 +0800

    修改生成核注清单表头SQL
    Signed-off-by: karyzeng <354697171@qq.com>

--- zzp.util/src/main/java/com/zw/invt/CreateInvtHeadSQL.java
+++ zzp.util/src/main/java/com/zw/invt/CreateInvtHeadSQL.java
@@ -19,7 +19,7 @@ public class CreateInvtHeadSQL {
             InvtHead invtHead = invtHeads.get(i);
             System.out.println("-- " + invtHead.getBondInvtNo() + " " + invtHead.getCitDocNo() + " " + invtHead.getEntBillNo());
             System.out.println("INSERT INTO `essdb`.`dcl_invt_head`(`company_code`, `company_uid`, `creater_id`, `creater_name`, `updater_id`, `updater_name`, `create_time`, `update_time`, `group_uid`, `group_name`, `bond_invt_no`) ");
-            System.out.println("VALUES ('" + invtHead.getCompanyCode() + "', '" + invtHead.getCompanyUid() + "', '8736', 'Chery', '8736', 'Chery', NOW(), NOW(), NULL, NULL, '" + invtHead.getBondInvtNo() + "');");
+            System.out.println("VALUES ('" + invtHead.getCompanyCode() + "', '" + invtHead.getCompanyUid() + "', '" + invtHead.getCreatorId() + "', '" + invtHead.getCreatorName() + "', '" + invtHead.getCreatorId() + "', '" + invtHead.getCreatorName() + "', NOW(), NOW(), NULL, NULL, '" + invtHead.getBondInvtNo() + "');");
             System.out.println();
         }
     }
@@ -28,10 +28,17 @@ public class CreateInvtHeadSQL {
         List<InvtHead> invtHeads = new ArrayList<InvtHead>();
         String path = CreateInvtHeadSQL.class.getClassLoader().getResource("invtHead.json").getPath();
         String s = CommonReadJsonUtil.readJsonFile(path);
-        JSONArray jsonArray = JSON.parseArray(s);
-        for (int i = 0; i < jsonArray.size(); i++) {
-            JSONObject jsonObject = jsonArray.getJSONObject(i);
+        JSONObject root = JSON.parseObject(s);
+        JSONObject baseDataJson = root.getJSONObject("baseData");
+        BaseData baseData = JSON.parseObject(baseDataJson.toString(), BaseData.class);
+        JSONArray list = root.getJSONArray("list");
+        for (int i = 0; i < list.size(); i++) {
+            JSONObject jsonObject = list.getJSONObject(i);
             InvtHead invtHead = JSON.parseObject(jsonObject.toString(), InvtHead.class);
+            invtHead.setCompanyUid(baseData.getCompanyUid());
+            invtHead.setCompanyCode(baseData.getCompanyCode());
+            invtHead.setCreatorId(baseData.getCreatorId());
+            invtHead.setCreatorName(baseData.getCreatorName());
             invtHeads.add(invtHead);
         }
         return invtHeads;

e0e4b920b6693566774514004d0360a0ce855caf
zzp <354697171@qq.com>
2020-06-07 22:47:53 +0800

    添加getEntBillHead方法

--- zzp.hoolinks.invt/src/main/java/com/zzp/invt/service/impl/EntBillHeadServiceImpl.java
+++ zzp.hoolinks.invt/src/main/java/com/zzp/invt/service/impl/EntBillHeadServiceImpl.java
@@ -1,10 +1,14 @@
 package com.zzp.invt.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.zzp.invt.entity.EntBillHead;
 import com.zzp.invt.mapper.EntBillHeadMapper;
 import com.zzp.invt.service.IEntBillHeadService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +21,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class EntBillHeadServiceImpl extends ServiceImpl<EntBillHeadMapper, EntBillHead> implements IEntBillHeadService {
 
+    @Autowired
+    private EntBillHeadMapper entBillHeadMapper;
+
+    public EntBillHead getEntBillHead(String companyUid, String docNo) {
+        QueryWrapper<EntBillHead> queryWrapper = new QueryWrapper<EntBillHead>();
+        queryWrapper.eq("company_uid", companyUid);
+        queryWrapper.eq("doc_no", docNo);
+        List<EntBillHead> entBillHeads = entBillHeadMapper.selectList(queryWrapper);
+        if (entBillHeads == null || entBillHeads.size() <= 0) {
+            return null;
+        }
+        return entBillHeads.get(0);
+    }
 }

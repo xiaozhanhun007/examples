d2f13df332a41c4322f8b4f937718d705caeccc7
zzp <354697171@qq.com>
2020-06-07 20:07:13 +0800

    添加listInvtDetails方法

--- zzp.hoolinks.invt/src/main/java/com/zzp/invt/service/impl/InvtDetailServiceImpl.java
+++ zzp.hoolinks.invt/src/main/java/com/zzp/invt/service/impl/InvtDetailServiceImpl.java
@@ -1,10 +1,14 @@
 package com.zzp.invt.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.zzp.invt.entity.InvtDetail;
 import com.zzp.invt.mapper.InvtDetailMapper;
 import com.zzp.invt.service.IInvtDetailService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +21,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class InvtDetailServiceImpl extends ServiceImpl<InvtDetailMapper, InvtDetail> implements IInvtDetailService {
 
+    @Autowired
+    private InvtDetailMapper invtDetailMapper;
+
+    public List<InvtDetail> listInvtDetails(Integer invtHeadId) {
+        QueryWrapper<InvtDetail> queryWrapper = new QueryWrapper<InvtDetail>();
+        queryWrapper.eq("invt_head_id", invtHeadId);
+        List<InvtDetail> invtDetails = invtDetailMapper.selectList(queryWrapper);
+        return invtDetails;
+    }
 }

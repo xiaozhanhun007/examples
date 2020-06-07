7f3f4196bb36aeeafff1b2b4c922f86f52eab127
zzp <354697171@qq.com>
2020-06-07 18:09:55 +0800

    添加createInvtHeadSQL和getResourceConfig方法

--- zzp.hoolinks.invt/src/main/java/com/zzp/invt/service/IInvtHeadService.java
+++ zzp.hoolinks.invt/src/main/java/com/zzp/invt/service/IInvtHeadService.java
@@ -3,6 +3,8 @@ package com.zzp.invt.service;
 import com.zzp.invt.entity.InvtHead;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.io.InputStream;
+
 /**
  * <p>
  * 核注清单表头 服务类
@@ -12,5 +14,9 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2020-06-07
  */
 public interface IInvtHeadService extends IService<InvtHead> {
+
+    void createInvtHeadSQL();
+
+    String getResourceConfig(InputStream resourceInputStream) throws Exception;
 
 }

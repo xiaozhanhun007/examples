c6839a4fc6f57b290de926c2a93c418543d4d5cd
karyzeng <354697171@qq.com>
2020-06-11 18:03:01 +0800

    重载listInvtDetails方法，添加filterPutrecSeqnos方法
    Signed-off-by: karyzeng <354697171@qq.com>

--- zzp.hoolinks.invt/src/main/java/com/zzp/invt/service/impl/InvtDetailServiceImpl.java
+++ zzp.hoolinks.invt/src/main/java/com/zzp/invt/service/impl/InvtDetailServiceImpl.java
@@ -1,10 +1,13 @@
 package com.zzp.invt.service.impl;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.zzp.invt.entity.InvtDetail;
+import com.zzp.invt.entity.InvtHead;
 import com.zzp.invt.mapper.InvtDetailMapper;
 import com.zzp.invt.service.IInvtDetailService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zzp.invt.service.IInvtHeadService;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,10 +27,44 @@ public class InvtDetailServiceImpl extends ServiceImpl<InvtDetailMapper, InvtDet
     @Autowired
     private InvtDetailMapper invtDetailMapper;
 
+    @Autowired
+    private IInvtHeadService invtHeadService;
+
     public List<InvtDetail> listInvtDetails(Integer invtHeadId) {
-        QueryWrapper<InvtDetail> queryWrapper = new QueryWrapper<InvtDetail>();
-        queryWrapper.eq("invt_head_id", invtHeadId);
+        return this.listInvtDetails(invtHeadId, null);
+    }
+
+    public List<InvtDetail> listInvtDetails(Integer invtHeadId, Integer putrecSeqno) {
+        LambdaQueryWrapper<InvtDetail> queryWrapper = new LambdaQueryWrapper<InvtDetail>();
+        queryWrapper.eq(InvtDetail::getInvtHeadId, invtHeadId);
+        if (putrecSeqno != null) {
+            queryWrapper.eq(InvtDetail::getPutrecSeqno, putrecSeqno);
+        }
         List<InvtDetail> invtDetails = invtDetailMapper.selectList(queryWrapper);
         return invtDetails;
+    }
+
+    @Override
+    public List<InvtDetail> listInvtDetails(String etpsInnerInvtNo, String companyUid, Integer putrecSeqno) {
+        InvtHead invtHead = invtHeadService.getInvtHeadByInnerInvtNo(etpsInnerInvtNo, companyUid);
+        if (invtHead == null) {
+            return null;
+        }
+        return this.listInvtDetails(invtHead.getId(), putrecSeqno);
+    }
+
+    @Override
+    public String filterPutrecSeqnos(List<InvtDetail> invtDetails) {
+        if (CollectionUtils.isEmpty(invtDetails)) {
+            return null;
+        }
+
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < invtDetails.size(); i++) {
+            sb.append(invtDetails.get(i).getPutrecSeqno() + ",");
+        }
+        sb.deleteCharAt(sb.lastIndexOf(","));
+
+        return sb.toString();
     }
 }

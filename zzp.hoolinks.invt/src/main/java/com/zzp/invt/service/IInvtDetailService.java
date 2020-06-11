13bec0248adc2a7fe4867a141dacfdc415026e49
karyzeng <354697171@qq.com>
2020-06-11 18:02:09 +0800

    重载listInvtDetails方法，添加filterPutrecSeqnos方法
    Signed-off-by: karyzeng <354697171@qq.com>

--- zzp.hoolinks.invt/src/main/java/com/zzp/invt/service/IInvtDetailService.java
+++ zzp.hoolinks.invt/src/main/java/com/zzp/invt/service/IInvtDetailService.java
@@ -16,10 +16,34 @@ import java.util.List;
 public interface IInvtDetailService extends IService<InvtDetail> {
 
     /**
-     * 根据表头Id查询标体列表
+     * 根据表头Id查询表体列表
      * @param invtHeadId 核注清单表头id
      * @return List<InvtDetail>
      */
     List<InvtDetail> listInvtDetails(Integer invtHeadId);
+
+    /**
+     * 根据表头id和备案序号来查询表体列表
+     * @param invtHeadId
+     * @param putrecSeqno
+     * @return
+     */
+    List<InvtDetail> listInvtDetails(Integer invtHeadId, Integer putrecSeqno);
+
+    /**
+     * 根据表头企业内部编号、企业UID和备案序号来查询表体列表
+     * @param etpsInnerInvtNo 表头企业内部编号
+     * @param companyUid 企业UID
+     * @param putrecSeqno 备案序号
+     * @return
+     */
+    List<InvtDetail> listInvtDetails(String etpsInnerInvtNo, String companyUid, Integer putrecSeqno);
+
+    /**
+     * 过滤得到表体的备案序号字符串，格式如1,2,3
+     * @param invtDetails
+     * @return
+     */
+    String filterPutrecSeqnos(List<InvtDetail> invtDetails);
 
 }
